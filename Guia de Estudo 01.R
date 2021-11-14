8b5b846b407caf3c7b59e86bd7efe2d98685ad2d
Marcos Cavalcanti <92671599+marcosaacavalcanti@users.noreply.github.com>
2021-11-14 16:13:47 -0300

    Add files via upload

---
+++ Guia de Estudo 01.R
@@ -0,0 +1,47 @@
+## Estudo de Caso 1
+
+## Questão 1
+
+## O arquivo .Rhistory salva todos os comandos digitados em uma sessão de uso.
+## Não, tal arquivo não salva o resultado de expressões digitas pelo usuário.
+## Sim, esse conteúdo é legível em um bloco de notas.
+
+
+## Questão 2
+
+## O tamanho do vetor do denominador é maior que o do numerador. Desse modo, não
+## é possível encontrar uma combinação exata de elementos para realizar a razão
+## entre os vetores.
+
+
+## Questão 3
+
+rep(seq(3,2,by = -.5),2,each = 2)
+
+
+## Questão 4
+
+(mean(rets)-selic)/sd(rets)
+
+
+## Questão 5
+
+(1/sqrt(2*pi))*exp(-1/2)
+
+
+## Questão 6
+
+raizes <- sqrt(1:1000)
+raizes[seq(2,1000,by = 2)]
+
+
+## Questão 7
+
+mean(raizes[raizes > 25])
+
+
+
+
+
+
+
---
+++ Guia de Estudo 02.R
@@ -0,0 +1,43 @@
+## Estudo de Caso 2
+
+## Questão 1
+
+pnorm(1.2)
+1 - pnorm(0.5)
+pnorm(1)-pnorm(-2.7)
+qnorm(0.25)
+dnorm(0)
+
+
+## Questão 2
+
+dbinom(2,5,0.5)
+pbinom(2,5,0.5)
+alfa <- qbinom(0.1875,5,0.5)
+
+## Questão 3
+
+ppois(3,2)
+
+
+## Questão 4
+
+1 - pexp(3)
+## O default do parâmetro "rate" implica em "rate = 1".
+
+
+## Questão 5
+
+mean(rnorm(1000))
+## E(X) = 0
+
+
+## Questão 6
+
+mean(rbinom(1000,5,0.5))
+## E(x) = 2.5
+
+
+
+
+
